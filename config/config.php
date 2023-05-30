e566ffdf9c5cb9f6579841deb2119e821c045efc
Alvilio Daras <18220011@respati.ac.id>
2023-05-30 12:27:17 +0700

    Upload 1

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc5205093ab4',
+  'passwordsalt' => '0368cdca737752b7530c5b54c61cb1',
+  'secret' => '6cc149b00129b9a9d29e1f283adbd695f573df10ca40f1a847af6119e41699a0bf6b5fe4b673cb76beb5c3d2f4f6946b',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => '5271-103-164-115-194.ngrok-free.app',
+  ),
+  'datadirectory' => 'C:\\xampp\\htdocs\\owncloud\\data',
+  'overwrite.cli.url' => 'http://localhost/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '7.0.15.2',
+  'dbname' => 'alviliodaras',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_alviliodaras',
+  'dbpassword' => '113190d3a5597c1ef2d3598412545f',
+  'installed' => true,
+);
